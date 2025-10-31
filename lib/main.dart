2641d833ae2a35a395976edbfc35283d0cdce8d1
verniiiii <vera.shestopalova@gmail.com>
2025-10-31 21:47:29 +0300

    GetIt is imported and an instance of TransactionRepository is registered as a singleton.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:get_it/get_it.dart';
 import 'core/app_router.dart';
-import 'core/transaction_inherited.dart';
 import 'features/transactions/data/transaction_repository.dart';
 
 void main() {
+  GetIt.I.registerSingleton<TransactionRepository>(TransactionRepository());
   runApp(MyApp());
 }
 
@@ -15,18 +16,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final repository = TransactionRepository(); // Создание единственного экземпляра репозитория
-    return TransactionInherited(
-      repository: repository,
-      child: MaterialApp.router(
-        title: 'Financial Tracker',
-        routerConfig: _router,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp.router(
+      title: 'Financial Tracker',
+      routerConfig: _router,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
