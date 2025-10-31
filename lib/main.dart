5f5353227ad13031a949227e11bf977c07aef8ca
verniiiii <vera.shestopalova@gmail.com>
2025-10-31 20:02:11 +0300

    Integrating TransactionInherited into MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'core/app_router.dart';
 import 'package:go_router/go_router.dart';
-
+import 'core/app_router.dart';
+import 'core/transaction_inherited.dart';
+import 'features/transactions/data/transaction_repository.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,14 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Financial Tracker',
-      routerConfig: _router,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
+    final repository = TransactionRepository(); // Создание единственного экземпляра репозитория
+    return TransactionInherited(
+      repository: repository,
+      child: MaterialApp.router(
+        title: 'Financial Tracker',
+        routerConfig: _router,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
